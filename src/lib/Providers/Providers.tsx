d0f4a9e2a4a7c454149b7ff88d5ea1dd5b23d192
Selim <selim@technovicinity.com>
2025-02-24 17:48:54 +0600

    configure redux toolkit

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -1,11 +1,16 @@
 "use client";
-
 import { ThemeProvider } from "@mui/material";
 import React from "react";
+import { Provider } from "react-redux";
 import { theme } from "../theme/theme";
+import { store } from "@/redux/store";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
+  );
 };
 
 export default Providers;
